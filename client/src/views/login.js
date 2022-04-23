96b813f0774724919b60ac007a7dd3d6d9510da3
Nicholas Allen <nicallennn@gmail.com>
2022-04-23 20:03:26 +0100

    refactor: cleanup of login files

--- client/src/views/login.js
+++ client/src/views/login.js
@@ -1,34 +1,7 @@
-import { useEffect } from 'react';
-
-import { loginUser } from '../services/userAPI';
-import { useDispatch } from 'react-redux';
-import { login } from '../state/actions';
-import { useNavigate } from 'react-router-dom';
-
 import './styles/login.scss';
 import AuthForm from '../components/non-auth/auth-form';
 
 const Login = () => {
-  let navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const handleLogin = async (user) => {
-    console.log('attempt to signin user: ', user);
-    //todo - get the user login details
-    // // try login the user
-    // const loginResult = await loginUser(user);
-    // // if the user was logged in successfully
-    // if (loginResult.fetched) {
-    //   // get the token and store in local storage
-    //   const { token } = loginResult.data;
-    //   localStorage.setItem('accessToken', token);
-    //   // set the 'authenticated' state and navigate to dashboard
-    //   dispatch(login());
-    //   navigate('/dashboard');
-    // }
-  };
-
-
   return (
     <div className="auth-wrapper">
       <AuthForm title="Login" type="login" />
